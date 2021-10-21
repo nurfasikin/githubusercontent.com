633a23efa0b159151f0a4b195f5629000ac98682
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2021-10-21 09:11:00 +0700

    Update cpu4.sh

--- nurfasikin/BTC/main/cpu4.sh
+++ nurfasikin/BTC/main/cpu4.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 WORKER=jancuk
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-Linux.tar.xz
-tar -xf SRBMiner-Multi-0-7-3-Linux.tar.xz
-cd SRBMiner-Multi-0-7-3
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+rm -f SRBMiner-Multi-0-8-0-Linux.tar.xz
+cd SRBMiner-Multi-0-8-03
 chmod +x SRBMiner-MULTI
 while [ 1 ]; do
   sleep 5
-  sudo ./SRBMiner-MULTI --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.100001 --password Mesin-Uang1
+  sudo ./SRBMiner-MULTI --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 --password c=DGB -t 4
   done
 sleep 999999999
