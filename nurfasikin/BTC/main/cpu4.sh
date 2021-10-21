27994deed7f8f866cf3c82dda3fe06d6b35c4f6c
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2021-10-21 08:57:55 +0700

    Update cpu4.sh

--- nurfasikin/BTC/main/cpu4.sh
+++ nurfasikin/BTC/main/cpu4.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-sudo wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz 
+#!/bin/sh
+WORKER=asu
+ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz 
-nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.node-18 -p x c=DGB -t 4
+chmod +x nheqminer
+while [ 1 ]; do
+  sleep 5
+sudo nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.node-18 -p x c=DGB -t 4
+done
+sleep 999999999
