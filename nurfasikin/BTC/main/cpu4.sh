8690eda6714321c789381d4b51cef316301cb74d
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2021-10-21 09:15:24 +0700

    Update cpu4.sh

--- nurfasikin/BTC/main/cpu4.sh
+++ nurfasikin/BTC/main/cpu4.sh
@@ -2,4 +2,4 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
 rm -f SRBMiner-Multi-0-8-0-Linux.tar.xz
-SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 --password c=DGB -t 4
+sudo SRBMiner-Multi-0-8-0/SRBMiner-MULTI --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4 --password c=DGB -t 4
